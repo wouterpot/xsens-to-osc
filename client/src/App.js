e88f1c5c1c6fd70ed5e9cfee6728598014ca68e7
Jeroen Koster <jeroen@baseflow.com>
2022-03-21 17:52:44 +0100

    Create reusable event handler

--- client/src/App.js
+++ client/src/App.js
@@ -97,45 +97,48 @@ function App() {
   );
 
 
-  const handleSensorSelect = (event, i) => {
-    config[i].sensor = event.target.value
-    console.log(config)
+  const updateColumn = (property, value, row) => {
+    if (!value) {
+      return
+    }
+    console.log(`updating ${property} of row ${row} from ${config[row][property]} to ${value}`)
+    config[row][property] = value
     setConfig([...config]);
   };
 
-  const handleDimensionSelect = (event, i) => {
-    config[i].dimension = event.target.value
-    console.log(config)
-    setConfig([...config]);
-  };
+  // const handleDimensionSelect = (event, i) => {
+  //   config[i].dimension = event.target.value
+  //   console.log(config)
+  //   setConfig([...config]);
+  // };
 
-  const handleChannelSelect = (event, i) => {
-    config[i].channel = event.target.value
-    console.log(config)
-    setConfig([...config]);
-  };
+  // const handleChannelSelect = (event, i) => {
+  //   config[i].channel = event.target.value
+  //   console.log(config)
+  //   setConfig([...config]);
+  // };
 
-  const handleSkipSelect = (event, i) => {
-    config[i].skip = event.target.value
-    console.log(config)
-    setConfig([...config]);
-  };
+  // const handleSkipSelect = (event, i) => {
+  //   config[i].skip = event.target.value
+  //   console.log(config)
+  //   setConfig([...config]);
+  // };
 
-  const handleOffsetSelect = (event, i) => {
-    config[i].offset = event.targetvalue
-    console.log(config)
-    setConfig([...config]);
-  }
-  const handleVelocitySelect = (event, i) => {
-    config[i].velocity = event.targetvalue
-    console.log(config)
-    setConfig([...config]);
-  }
-  const handleTresholdSelect = (event, i) => {
-    config[i].treshold = event.targetvalue
-    console.log(config)
-    setConfig([...config]);
-  }
+  // const handleOffsetSelect = (event, i) => {
+  //   config[i].offset = event.targetvalue
+  //   console.log(config)
+  //   setConfig([...config]);
+  // }
+  // const handleVelocitySelect = (event, i) => {
+  //   config[i].velocity = event.targetvalue
+  //   console.log(config)
+  //   setConfig([...config]);
+  // }
+  // const handleTresholdSelect = (event, i) => {
+  //   config[i].treshold = event.targetvalue
+  //   console.log(config)
+  //   setConfig([...config]);
+  // }
 
   return (
     <div className="App">
@@ -169,7 +172,7 @@ function App() {
 
                 <TableRow key={i} {...row.getRowProps()}>
                   <TableCell key="0">
-                    <Select value={config[i]?.sensor} onChange={((e) => handleSensorSelect(e, i))}>
+                    <Select value={config[i]?.sensor} onChange={((e) => updateColumn('sensor',e.target.value, i))}>
                       {
                         sensors.map((sensor, k) =>
                           <MenuItem value={sensor} key={k}>{sensor}</MenuItem>
@@ -178,15 +181,12 @@ function App() {
                     </Select>
                   </TableCell>
 
-
                   <TableCell key="1">
-                  <TableCell key="3">
-                    <input type="number" name="channel" value={config[i]?.channel} onChange={((e) => handleChannelSelect(e, i))} />
-                  </TableCell>
+                    <input type="number" name="channel" value={config[i]?.channel} onChange={((e) => updateColumn('channel',e.target.value, i))} />
                   </TableCell>
 
                   <TableCell key="2">
-                    <Select value={config[i]?.dimension} onChange={((e) => handleDimensionSelect(e, i))}>
+                    <Select value={config[i]?.dimension} onChange={((e) => updateColumn('dimension',e.target.value, i))}>
                       {
                         dimensions.map((dimension, k) =>
                           <MenuItem value={dimension} key={k}>{dimension}</MenuItem>
@@ -196,19 +196,19 @@ function App() {
                   </TableCell>
 
                   <TableCell key="3">
-                    <input type="number" name="skip" value={config[i]?.skip || 0 } onChange={((e) => handleSkipSelect(e, i))} />
+                    <input type="number" name="skip" value={config[i]?.skip || 0 } onChange={((e) => updateColumn('skip',e.target.value, i))}/>
                   </TableCell>
 
                   <TableCell key="4">
-                    <input type="number" name="offset" value={config[i]?.offset || 0 } onChange={((e) => handleOffsetSelect(e, i))} />
+                    <input type="number" name="offset" value={config[i]?.offset || 0 } onChange={((e) => updateColumn('offset',e.target.value, i))}/>
                   </TableCell>
 
                   <TableCell key="5">
-                    <input type="number" name="velocity" value={config[i]?.velocity || 0 } onChange={((e) => handleVelocitySelect(e, i))} />
+                    <input type="number" name="velocity" value={config[i]?.velocity || 0 } onChange={((e) => updateColumn('velocity',e.target.value, i))}/>
                   </TableCell>
 
                   <TableCell key="6">
-                    <input type="number" name="treshold" value={config[i]?.treshold || 0 } onChange={((e) => handleTresholdSelect(e, i))} />
+                    <input type="number" name="treshold" value={config[i]?.treshold || 0 } onChange={((e) => updateColumn('treshold',e.target.value, i))}/>
                   </TableCell>
 
                 </TableRow>
